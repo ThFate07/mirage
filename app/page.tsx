b11df54714b40f668f5aadcd2b8719503aa83310
Dhaval-Suthar <virexela@icloud.com>
2024-09-28 19:04:05 +0530

    Enhance video component performance and optimize rendering logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,84 +1,127 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { use, useEffect, useRef, useState } from "react";
 
-interface VideoInfoProps {
-  video: HTMLVideoElement | null; // video can be an HTMLVideoElement or null
-  fileSizeInBytes: React.MutableRefObject<number>;
+interface VideoData {
+  codec: string;
+  fileSize: number;
   bitrateData: { timestamp: number; bitrate: number }[];
 }
 
 export default function Home() {
   const [video, setVideo] = useState("");
-  const [processedVideo, setProcessedVideo] = useState("");
+  const [videoData, setVideoData] = useState(null as VideoData | null);
 
-  const [rerender, setReRender] = useState(false);
+  const [processedVideo, setProcessedVideo] = useState("");
+  const [processedVideoData, setProcessedVideoData] = useState(null as VideoData | null);
 
   const videoRef = useRef<HTMLVideoElement>(null);
   const processedVideoRef = useRef<HTMLVideoElement>(null);
-
   const input = useRef<HTMLInputElement>(null);
   const fileSizeInBytes = useRef(0);
-
-  const bitrateData = [
-    { timestamp: 0, bitrate: 1000 },
-    { timestamp: 1, bitrate: 2000 },
-    { timestamp: 2, bitrate: 3000 },
-    { timestamp: 3, bitrate: 4000 },
-    { timestamp: 4, bitrate: 5000 },
-    { timestamp: 5, bitrate: 6000 },
-    { timestamp: 6, bitrate: 7000 },
-    { timestamp: 7, bitrate: 8000 },
-    { timestamp: 8, bitrate: 9000 },
-    { timestamp: 9, bitrate: 10000 },
-    { timestamp: 10, bitrate: 11000 },
-    { timestamp: 11, bitrate: 12000 },
-    { timestamp: 12, bitrate: 13000 },
-    { timestamp: 13, bitrate: 14000 },
-    { timestamp: 14, bitrate: 15000 },
-    { timestamp: 15, bitrate: 16000 },
-    { timestamp: 16, bitrate: 17000 },
-    { timestamp: 17, bitrate: 18000 },
-    { timestamp: 18, bitrate: 19000 },
-    { timestamp: 19, bitrate: 20000 },
-    { timestamp: 20, bitrate: 21000 },
-    { timestamp: 21, bitrate: 22000 },
-    { timestamp: 22, bitrate: 23000 },
-    { timestamp: 23, bitrate: 24000 },
-    { timestamp: 24, bitrate: 25000 },
-    { timestamp: 25, bitrate: 26000 },
-    { timestamp: 26, bitrate: 27000 },
-    { timestamp: 27, bitrate: 28000 },
-    { timestamp: 28, bitrate: 29000 },
-    { timestamp: 29, bitrate: 30000 },
-    { timestamp: 30, bitrate: 31000 },
-    { timestamp: 31, bitrate: 32000 },
-    { timestamp: 32, bitrate: 33000 },
-    { timestamp: 33, bitrate: 34000 },
-    { timestamp: 34, bitrate: 35000 },
-    { timestamp: 35, bitrate: 36000 },
-    { timestamp: 36, bitrate: 37000 },
-    { timestamp: 37, bitrate: 38000 },
-    { timestamp: 38, bitrate: 39000 },
-    { timestamp: 39, bitrate: 40000 },
-    { timestamp: 40, bitrate: 41000 },
-  ];
 
   const processVideo = async (url: string) => {
     await new Promise((resolve) => setTimeout(resolve, 5000));
     setProcessedVideo(url);
   };
 
-  const handleMetadataLoaded = () => {
+  useEffect(() => {
     if (videoRef.current) {
-      setReRender(!rerender);
+      setVideoData({
+        codec: "...",
+        fileSize: fileSizeInBytes.current,
+        bitrateData: [
+          { timestamp: 0, bitrate: 1000 },
+          { timestamp: 1, bitrate: 2000 },
+          { timestamp: 2, bitrate: 3000 },
+          { timestamp: 3, bitrate: 4000 },
+          { timestamp: 4, bitrate: 5000 },
+          { timestamp: 5, bitrate: 6000 },
+          { timestamp: 6, bitrate: 7000 },
+          { timestamp: 7, bitrate: 8000 },
+          { timestamp: 8, bitrate: 9000 },
+          { timestamp: 9, bitrate: 10000 },
+          { timestamp: 10, bitrate: 11000 },
+          { timestamp: 11, bitrate: 12000 },
+          { timestamp: 12, bitrate: 13000 },
+          { timestamp: 13, bitrate: 14000 },
+          { timestamp: 14, bitrate: 15000 },
+          { timestamp: 15, bitrate: 16000 },
+          { timestamp: 16, bitrate: 17000 },
+          { timestamp: 17, bitrate: 18000 },
+          { timestamp: 18, bitrate: 19000 },
+          { timestamp: 19, bitrate: 20000 },
+          { timestamp: 20, bitrate: 21000 },
+          { timestamp: 21, bitrate: 22000 },
+          { timestamp: 22, bitrate: 23000 },
+          { timestamp: 23, bitrate: 24000 },
+          { timestamp: 24, bitrate: 25000 },
+          { timestamp: 25, bitrate: 26000 },
+          { timestamp: 26, bitrate: 27000 },
+          { timestamp: 27, bitrate: 28000 },
+          { timestamp: 28, bitrate: 29000 },
+          { timestamp: 29, bitrate: 30000 },
+          { timestamp: 30, bitrate: 31000 },
+          { timestamp: 31, bitrate: 32000 },
+          { timestamp: 32, bitrate: 33000 },
+          { timestamp: 33, bitrate: 34000 },
+          { timestamp: 34, bitrate: 35000 },
+          { timestamp: 35, bitrate: 36000 },
+          { timestamp: 36, bitrate: 37000 },
+          { timestamp: 37, bitrate: 38000 },
+        ],
+      });
     }
-  };
+  }, [video]);
+
+  useEffect(() => {
+    if (processedVideoRef.current) {
+      setProcessedVideoData({
+        codec: "...",
+        fileSize: fileSizeInBytes.current,
+        bitrateData: [
+          { timestamp: 0, bitrate: 1000 },
+          { timestamp: 1, bitrate: 2000 },
+          { timestamp: 2, bitrate: 3000 },
+          { timestamp: 3, bitrate: 4000 },
+          { timestamp: 4, bitrate: 5000 },
+          { timestamp: 5, bitrate: 6000 },
+          { timestamp: 6, bitrate: 7000 },
+          { timestamp: 7, bitrate: 8000 },
+          { timestamp: 8, bitrate: 9000 },
+          { timestamp: 9, bitrate: 10000 },
+          { timestamp: 10, bitrate: 11000 },
+          { timestamp: 11, bitrate: 12000 },
+          { timestamp: 12, bitrate: 13000 },
+          { timestamp: 13, bitrate: 14000 },
+          { timestamp: 14, bitrate: 15000 },
+          { timestamp: 15, bitrate: 16000 },
+          { timestamp: 16, bitrate: 17000 },
+          { timestamp: 17, bitrate: 18000 },
+          { timestamp: 18, bitrate: 19000 },
+          { timestamp: 19, bitrate: 20000 },
+          { timestamp: 20, bitrate: 21000 },
+          { timestamp: 21, bitrate: 22000 },
+          { timestamp: 22, bitrate: 23000 },
+          { timestamp: 23, bitrate: 24000 },
+          { timestamp: 24, bitrate: 25000 },
+          { timestamp: 25, bitrate: 26000 },
+          { timestamp: 26, bitrate: 27000 },
+          { timestamp: 27, bitrate: 28000 },
+          { timestamp: 28, bitrate: 29000 },
+          { timestamp: 29, bitrate: 30000 },
+          { timestamp: 30, bitrate: 31000 },
+          { timestamp: 31, bitrate: 32000 },
+          { timestamp: 32, bitrate: 33000 },
+        ],
+      });
+    }
+  }, [processedVideo]);
 
   return (
     <div className="antialiased bg-gray-50 dark:bg-gray-900">
       <main className="p-4 h-auto pt-20">
-        <div className=" rounded-lg border-gray-300 dark:border-gray-600 h-60 mb-4">
+        <div className=" rounded-lg border-gray-300 dark:border-gray-600 h-60 mb-20 md:mb-4">
           <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
             <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Mirage</span>{" "}
             AI.
@@ -96,7 +139,6 @@ export default function Home() {
               <button
                 onClick={() => {
                   input.current && input.current.click();
-                  setProcessedVideo("");
                 }}
               >
                 <svg
@@ -139,15 +181,9 @@ export default function Home() {
           </div>
         </div>
         <div className="grid grid-cols-2 gap-4 mb-4">
-          <div className=" border-gray-300 dark:border-gray-600 h-96">
+          <div className=" border-gray-300 dark:border-gray-600 h-20 md:h-96">
             {video ? (
-              <video
-                className="w-full h-full"
-                controls
-                src={video}
-                ref={videoRef}
-                onLoadedMetadata={handleMetadataLoaded}
-              ></video>
+              <video className="w-full h-full" controls src={video} autoPlay ref={videoRef}></video>
             ) : (
               <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-full flex justify-center">
                 <button
@@ -168,6 +204,11 @@ export default function Home() {
                 fileSizeInBytes.current = file.size;
 
                 const url = URL.createObjectURL(file);
+                if (video) {
+                  setVideo("");
+                  setProcessedVideo("");
+                }
+
                 setVideo(url);
                 processVideo(url);
               }
@@ -177,19 +218,13 @@ export default function Home() {
             type="file"
             accept="video/*"
           />
-          <div className="h-96">
+          <div className="h-20 md:h-96">
             {video ? (
               processedVideo ? (
-                <video
-                  className="w-full h-full"
-                  controls
-                  src={processedVideo}
-                  ref={processedVideoRef}
-                  onLoadedMetadata={handleMetadataLoaded}
-                ></video>
+                <video className="w-full h-full" controls src={processedVideo} autoPlay ref={processedVideoRef}></video>
               ) : (
                 <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-full flex justify-center">
-                  <h3 className=" my-auto text-slate-100">Proccessing...</h3>
+                  <img className=" h-20 md:h-40 my-auto" src="./loader.gif" alt="" />
                 </div>
               )
             ) : (
@@ -198,12 +233,11 @@ export default function Home() {
               </div>
             )}
           </div>
-          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72 p-4">
-            <VideoStats video={videoRef.current} bitrateData={bitrateData} fileSizeInBytes={fileSizeInBytes} />
+          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-full p-4">
+            {videoData && <VideoStats data={videoData} video={videoRef.current} />}
           </div>
-          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72 p-4">
-            {/* need to replace video with processed video ref  */}
-            <VideoStats video={processedVideoRef.current} bitrateData={bitrateData} fileSizeInBytes={fileSizeInBytes} />
+          <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-full p-4">
+            {processedVideoData && <VideoStats data={processedVideoData} video={processedVideoRef.current} />}
           </div>
         </div>
       </main>
@@ -211,19 +245,16 @@ export default function Home() {
   );
 }
 
-const VideoStats: React.FC<VideoInfoProps> = ({ video, bitrateData, fileSizeInBytes }) => {
-  if (!video) return null;
-
-  const [resolution, setResolution] = useState({ width: 0, height: 0 });
-  const [duration, setDuration] = useState(0);
-  const [currentBitrate, setCurrentBitrate] = useState<number>(0);
+const VideoStats = ({ data, video }: any) => {
+  if (!data || !video) return null;
+  const [currentBitrate, setCurrentBitrate] = useState(0);
 
   const updateBitrate = () => {
     // Get the current time of the video without decimals
     const currentTime = Math.floor(video.currentTime);
 
     // Find the bitrate based on the current timestamp
-    const bitrate = bitrateData.find(
+    const bitrate = data.bitrateData.find(
       (data: { timestamp: Number; bitrate: number }) => data.timestamp === currentTime
     )?.bitrate;
 
@@ -234,42 +265,28 @@ const VideoStats: React.FC<VideoInfoProps> = ({ video, bitrateData, fileSizeInBy
   useEffect(() => {
     video.addEventListener("timeupdate", updateBitrate);
 
-    setResolution({
-      width: video.videoWidth,
-      height: video.videoHeight,
-    });
-    setDuration(video.duration);
-
     return () => {
       video.removeEventListener("timeupdate", updateBitrate);
     };
   }, [video]);
 
-  useEffect(() => {}, [video]);
   return (
     <>
-      {/* <div>
-        <p>Codec: </p>
-        <p>File Size: {(fileSizeInBytes.current / 1024 / 1024).toFixed(3)} MB</p>
-        <p>
-          Resolution: {resolution.height} x {resolution.width}
-        </p>
-        <p>Duration: {duration > 60 ? (duration / 60).toString() + " Minute" : duration.toString() + " Seconds"} </p>
-        <p>Bitrate: {currentBitrate} Kbps</p>
-      </div> */}
-
       <ul className="space-y-4 text-left text-gray-500 dark:text-gray-400">
         <li className="flex items-center space-x-3 rtl:space-x-reverse">
+          Codec: <span className="font-semibold px-1 text-gray-900 dark:text-white">...</span>
+        </li>
+        <li className="flex items-center space-x-3 rtl:space-x-reverse">
           File Size:{" "}
-          <span className="font-semibold px-1 text-gray-900 dark:text-white">
-            {(fileSizeInBytes.current / 1024 / 1024).toFixed(3)} MB
+          <span className="font-semibold  text-gray-900 dark:text-white">
+            {(data.fileSize / 1024 / 1024).toFixed(3)} MB
           </span>
         </li>
         <li className="flex items-center space-x-3 rtl:space-x-reverse">
           <span>
             Resolution:{" "}
             <span className="font-semibold text-gray-900 dark:text-white">
-              {resolution.height} x {resolution.width}
+              {video.videoHeight} x {video.videoWidth}
             </span>
           </span>
         </li>
@@ -277,18 +294,15 @@ const VideoStats: React.FC<VideoInfoProps> = ({ video, bitrateData, fileSizeInBy
           <span>
             Duration:{" "}
             <span className="font-semibold text-gray-900 dark:text-white">
-              {duration > 60 ? (duration / 60).toString() + " Minute" : duration.toString() + " Seconds"}
+              {video.duration > 60
+                ? (video.duration / 60).toString() + " Minute"
+                : video.duration.toString() + " Seconds"}
             </span>
           </span>
         </li>
         <li className="flex items-center space-x-3 rtl:space-x-reverse">
           <span>
             Bitrate: <span className="font-semibold text-gray-900 dark:text-white">{currentBitrate} Kbps</span>
-          </span>
-        </li>
-        <li className="flex items-center space-x-3 rtl:space-x-reverse">
-          <span>
-            Free updates: <span className="font-semibold text-gray-900 dark:text-white">6 months</span>
           </span>
         </li>
       </ul>
